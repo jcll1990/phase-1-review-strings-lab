5740379ca1b2174e5c396ee85c412500f821d456
Juan Carlos Lopez Larco <jclopezlarco@gmail.com>
2023-08-21 16:56:26 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+const currentUser = "JC";
+
+const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser + "!";
+
+
+const firstInitial = currentUser[0];
+const shortGreeting = "Welcome, " + firstInitial + "!";
